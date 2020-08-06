79b638261af14eeb87c6f2a04cdcf6ffa4bff2d9
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-06 04:32:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
-  def initialize(brand)
-    @brand = brand
-  end
+  
 
   attr_reader :brand
   attr_accessor :color, :size, :material, :condition
-
+def initialize(brand)
+    @brand = brand
+  end
   def cobble()
     self.condition = "new"
     puts "Your shoe is as good as new!"
