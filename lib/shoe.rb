57583068141ef9a47c21825a967a12c2f0c69e06
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-06 04:27:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :brand
+ 
+  def initialize(brand)
+    @title = title
+  end
+ 
+  def cobble
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
