12296067ab6d9bccaedb57f9dc4ff5732a23337e
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-06 04:30:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,12 @@ class Book
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
  
-  def initialize(brand, condition = "New")
+  def initialize(brand)
     @brand = brand
   end
  
   def cobble
+    self.condition = "new"
     puts "The show has been repaired"
   end
  
